b9bab04860933938ce7261f0e1f8bc1cfbdce8fa
ceeoreo <ceoraford@gmail.com>
2024-03-21 11:03:30 -0400

    Update button

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,9 +1,10 @@
 import Link from "next/link"
+import ButtonStyles from "../styles/Button.module.css"
 
 export default function LogoutButton() {
   return (
     <Link href="/api/auth/logout"> 
-      <button> Logout</button>
+      <button className={ButtonStyles.button}> Logout</button>
     </Link>
   )
 }
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,5 @@
+'use client';
+
 import NavBar from "@/components/NavBar";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Image from "next/image";
@@ -12,9 +14,10 @@ export default function Profile(){
     user && (
       <div>
         <NavBar />
-        <Image src={user.picture} alt={user.name} />
+        {/* <Image src={user.picture} alt={user.name} width={50} height={50}/> */}
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <h2> Courses Completed</h2>
       </div>
     )
   )
